3679c33dcc7ea774f08596703bf3433600ca005f
Kabir Namdeo <81280712+KabirNamdeo@users.noreply.github.com>
2025-11-25 18:55:13 +0530

    initial: project skeleton

---
+++ src/main/java/com/kabir/student/StudentManagementCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.kabir.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementCrudApiApplication.class, args);
+	}
+
+}
